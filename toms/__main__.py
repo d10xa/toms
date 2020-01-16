a228780cee23a7f2c536c6b73e5463a7c1e69f05
d10xa <d10xa@mail.ru>
2020-01-17 00:45:38 +0300

    Add input from stdin

--- toms/__main__.py
+++ toms/__main__.py
@@ -1,9 +1,18 @@
 import sys
+from typing import Iterator
+
 from toms import convert_toms_str
+
+
+def flat_stdin() -> Iterator[str]:
+    import itertools
+    return itertools.chain.from_iterable([i.split() for i in sys.stdin])
 
 
 def main():
     args = sys.argv[1:]
+    if len(args) == 0:
+        args = list(flat_stdin())
     for arg in args:
         print(convert_toms_str(arg))
 
