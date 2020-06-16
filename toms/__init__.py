286e94ed78742e5183015d00ee05fada0dd6d0a8
Your Name <you@example.com>
2020-06-16 22:38:41 +0300

    v0.4.4

--- toms/__init__.py
+++ toms/__init__.py
@@ -2,5 +2,5 @@ from .toms import convert_toms_str
 from .toms import replace_millis_str
 from .toms import split_timestamp
 
-__version__ = '0.4.2'
+__version__ = '0.4.4'
 __all__ = ['convert_toms_str', 'split_timestamp', 'replace_millis_str']
