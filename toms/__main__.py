edfd5240d713a42e51e2f48816d7e594bbbb14f9
Your Name <you@example.com>
2020-06-17 21:38:52 +0300

    Fix reading arguments from stdin

--- toms/__main__.py
+++ toms/__main__.py
@@ -11,16 +11,21 @@ def flat_stdin() -> Iterator[str]:
 
 
 def main():
-    if sys.argv[1] == "now":
+    first_arg = sys.argv[1] if 1 < len(sys.argv) else None
+    if first_arg == "now":
         import time
         millis = int(round(time.time() * 1000))
         print(millis)
-    elif sys.argv[1] == "replace":
+    elif first_arg == "replace":
         for x in sys.stdin:
             sys.stdout.write(replace_millis_str(x))
     else:
-        args = (iter(sys.argv[1:]) or flat_stdin())
-        for x in (convert_toms_str(r) for r in args):
+        args_list = sys.argv[1:]
+        if len(args_list) == 0:
+            input_iter = flat_stdin()
+        else:
+            input_iter = iter(args_list)
+        for x in (convert_toms_str(r) for r in input_iter):
             print(x)
 
 
